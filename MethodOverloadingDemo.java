de23dcfc39f8c0ebba75a9c3cec232e66d85d24d
Vaishnavi20217 <vaishubunny217@gmail.com>
2026-02-18 12:00:12 +0530

    Add files via upload

---
+++ MethodOverloadingDemo.java
@@ -0,0 +1,30 @@
+class MethodOverLoadingDemo
+{
+}
+{
+
+public float sum(float n1,float n2)
+{
+return n1 + n2;
+}
+
+public double sum(double n1,double n2,double n3)
+{
+return n1 + n2 + n3;
+}
+
+
+	public static void main(String[] args) 
+	{
+MethodOverLoadingDemo obj = new MethodOverLoadingDemo();
+
+float result1 = obj.sum(10,20);
+System.out.println("Result of 2 Numbers "+ result1);
+
+double result2 = obj.sum(5,15,25);
+System.out.println("Result of 3 Numbers "+ result2);
+
+
+
+	}
+}
